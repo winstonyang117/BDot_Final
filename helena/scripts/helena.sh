9e23036547f75a629d6a70f46688fe5c99214de5
hxmeng-hub <64174229+hxmeng-hub@users.noreply.github.com>
2020-06-26 00:01:52 -0400

    Update helena.sh

--- helena/scripts/helena.sh
+++ helena/scripts/helena.sh
@@ -4,7 +4,8 @@ set -x
 set -e
 
 sudo apt-get update
-sudo apt-get install build-essential python-dev libatlas-base-dev -y
+sudo apt upgrade
+sudo apt-get install build-essential python-dev libatlas-base-dev gfortran -y
 
 # uninstall pip3 version of numpy/scipy and install rpi specific packages 
 sudo pip3 uninstall numpy  
@@ -15,5 +16,6 @@ sudo apt install python3-scipy -y
 # for config and start/stop influxdb docker container
 sudo apt install docker-compose -y
 
-sudo pip3 install influxdb nitime pandas netifaces configobj psutil cryptography pyinstaller statsmodels
+sudo pip3 install influxdb nitime pandas netifaces configobj psutil cryptography pyinstaller 
+sudo pip3 install statsmodels
 
