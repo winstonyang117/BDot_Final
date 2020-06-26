a9fbf3e32bc846fb241efef7308a34bd3ac4903e
Raspberry-Shake User <myshake@raspberryshake>
2020-06-26 16:40:31 +0000

    script for setup dev

--- helena/scripts/helena.sh
+++ helena/scripts/helena.sh
@@ -4,18 +4,26 @@ set -x
 set -e
 
 sudo apt-get update
-sudo apt upgrade
-sudo apt-get install build-essential python-dev libatlas-base-dev gfortran -y
+sudo apt upgrade -y
+sudo apt install python3.7
+
+sudo apt-get install build-essential python-dev libatlas-base-dev -y
 
 # uninstall pip3 version of numpy/scipy and install rpi specific packages 
 sudo pip3 uninstall numpy  
 sudo apt install python3-numpy -y
 sudo pip3 uninstall scipy  
 sudo apt install python3-scipy -y
+sudo pip3 uninstall pandas
+sudo apt install python3-pandas -y
+sudo pip3 uninstall tornado 
+sudo apt install python3-tornado -y
 
 # for config and start/stop influxdb docker container
 sudo apt install docker-compose -y
 
-sudo pip3 install influxdb nitime pandas netifaces configobj psutil cryptography pyinstaller 
-sudo pip3 install statsmodels
+sudo pip3 install influxdb nitime netifaces configobj psutil cryptography pyinstaller 
+sudo apt install python3-statsmodels -y
+
+sudo apt autoremove -y
 
