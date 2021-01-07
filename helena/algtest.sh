c0f5c1f45da92c3bfdd05d2be9ae10fb2284d521
wsonguga <wsong@JSong.local>
2021-01-07 15:49:26 -0500

    add more beds

--- helena/algtest.sh
+++ helena/algtest.sh
@@ -2,6 +2,15 @@
 nohup ./algtest.py  b8:27:eb:dc:80:5c &
 nohup ./algtest.py  b8:27:eb:76:15:20 &
 nohup ./algtest.py  b8:27:eb:a1:89:49 &
-#BedD
-nohup ./algtest.py b8:27:eb:17:a0:48 &
+#song
+nohup ./algtest.py b8:27:eb:17:a0:48 & # BedD
+nohup ./algtest.py b8:27:eb:7d:dd:a4 & # BedW
+nohup ./algtest.py b8:27:eb:97:f5:ac & # BedJ
+nohup ./algtest.py b8:27:eb:66:b8:3d & # BedA
+
+#CTRU
+nohup ./algtest.py b8:27:eb:6c:6e:22 & #H1
+nohup ./algtest.py b8:27:eb:5b:35:37 & #H2
+nohup ./algtest.py b8:27:eb:23:4b:2b & #F1
+nohup ./algtest.py b8:27:eb:80:1c:cf & #F2
 
