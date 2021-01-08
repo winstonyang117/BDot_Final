3d813eac1da490b88d64d843ed60e70d014eb2f7
wsonguga <wsong@JSong.local>
2021-01-08 17:17:41 -0500

    update

--- helena/algtest.sh
+++ helena/algtest.sh
@@ -1,16 +1,20 @@
 #caihong
-nohup ./algtest.py  b8:27:eb:dc:80:5c &
-nohup ./algtest.py  b8:27:eb:76:15:20 &
-nohup ./algtest.py  b8:27:eb:a1:89:49 &
+nohup ./algtest.py  b8:27:eb:dc:80:5c & #QiaoXF
+# nohup ./algtest.py  b8:27:eb:76:15:20 & #HuangPH
+nohup ./algtest.py  b8:27:eb:a1:89:49 & #JiangXL
+# nohup ./algtest.py  b8:27:eb:97:a6:73 &
+nohup ./algtest.py  b8:27:eb:b2:ab:05 &
+# nohup ./algtest.py  b8:27:eb:ca:2a:5f &
+
 #song
-nohup ./algtest.py b8:27:eb:17:a0:48 & # BedD
-nohup ./algtest.py b8:27:eb:7d:dd:a4 & # BedW
-nohup ./algtest.py b8:27:eb:97:f5:ac & # BedJ
-nohup ./algtest.py b8:27:eb:66:b8:3d & # BedA
+nohup ./algtest.py b8:27:eb:17:a0:48 & #BedD
+# nohup ./algtest.py b8:27:eb:7d:dd:a4 & #BedW
+nohup ./algtest.py b8:27:eb:97:f5:ac & #BedJ
+# nohup ./algtest.py b8:27:eb:66:b8:3d & #BedA
 
 #CTRU
-nohup ./algtest.py b8:27:eb:6c:6e:22 & #H1
-nohup ./algtest.py b8:27:eb:5b:35:37 & #H2
-nohup ./algtest.py b8:27:eb:23:4b:2b & #F1
-nohup ./algtest.py b8:27:eb:80:1c:cf & #F2
+# nohup ./algtest.py b8:27:eb:6c:6e:22 & #H1
+# nohup ./algtest.py b8:27:eb:5b:35:37 & #H2
+# nohup ./algtest.py b8:27:eb:23:4b:2b & #F1
+# nohup ./algtest.py b8:27:eb:80:1c:cf & #F2
 
