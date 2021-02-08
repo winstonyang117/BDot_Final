f7d48530968bab90f8ab604e4ac7e866c60ea3fb
Raspberry-Shake User <myshake@raspberryshake>
2021-02-08 19:40:14 +0000

    allow quick deployment

--- packupdate.sh
+++ packupdate.sh
@@ -41,6 +41,15 @@ date -r helena/bin/main > version.txt
 #                         update.sh  \
 #                         version.txt 
 
-scp -i ~/homedots.pem build/beddot.tar.gz ubuntu@homedots.us:/var/www/html/bdsoftware/
-scp -i ~/homedots.pem version.txt ubuntu@homedots.us:/var/www/html/bdsoftware/rversion.txt
-scp -i ~/homedots.pem list.txt ubuntu@homedots.us:/var/www/html/bdsoftware/list.txt
+# scp -i ~/homedots.pem build/beddot.tar.gz ubuntu@homedots.us:/var/www/html/bdsoftware/
+# scp -i ~/homedots.pem version.txt ubuntu@homedots.us:/var/www/html/bdsoftware/rversion.txt
+# scp -i ~/homedots.pem list.txt ubuntu@homedots.us:/var/www/html/bdsoftware/list.txt
+
+declare -a iplist=("10.0.0.84" "10.0.0.212" "10.0.0.223" "10.0.0.225")
+
+for ip in "${iplist[@]}"
+do
+   echo "set up: $ip"
+   scp -i ~/bdot.pem beddot.tar.gz  myshake@$ip:/opt/ && ssh -i ~/bdot.pem -t myshake@$ip 'sudo systemctl stop helena && sudo systemctl stop influxshake && cd /opt/ && tar -xvf beddot.tar.gz && sudo systemctl start influxshake && sudo systemctl start helena'
+   # ssh -i ~/bdot.pem -t myshake@$ip 'cd /opt/RaspiWiFi/libs/reset_device/ && sudo python3 manual_reset.py'
+done
